674968cdd60f457d870d20611cbbb08b1b131932
Phong <90952598+phongthanhh@users.noreply.github.com>
2023-02-10 13:12:17 +0700

    update folder

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,6 +1,12 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
     // Chứa reducer
 })
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware));
+
+export const store = createStore(rootReducer, applyMiddleware(thunk))
